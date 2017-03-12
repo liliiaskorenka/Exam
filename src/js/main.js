92e957f2a71360b07225b0bbe22fbba48fcf980b
Lilia Skorenka <skorenjka@gmail.com>
2017-03-12 23:20:27 +0200

    With adaption

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,18 @@ $.validate({
         return false;
     }
 });
+
+
+    $(function() {
+        $("#map").googleMap();
+        $("#map").addWay({
+            start: "15 avenue des champs Elysées 75008 Paris", // Postal address for the start marker (obligatory)
+            end:  [48.895651, 2.290569], // Postal Address or GPS coordinates for the end marker (obligatory)
+            route : 'way', // Block's ID for the route display (optional)
+            langage : 'english', // language of the route detail (optional)
+            step: [ // Array of steps (optional)
+                [48.85837009999999, 2.2944813000000295], // Postal Address or GPS coordinates of the step
+                "Porte Maillot, 75017 Paris", // Postal Address or GPS coordinates of the step
+            ]
+        });
+    });
